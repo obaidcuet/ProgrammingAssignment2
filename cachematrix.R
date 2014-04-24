c82a7e55e56c4fb82ebb428219ecc873564dc8a3
obaidcuet <obaidcuet@gmail.com>
2014-04-25 00:06:44 +0800

    cachematrix.R, for 'Peer Assessments' of 'R Programming' course, fix indent

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
-## This R script has two below functions:
-## "makeCacheMatrix": it will create a special matrix with functions to access/update values of the matrix
-## "cacheSolve": it will produce and store inverse of the matrix produced by "makeCacheMatrix" function
+## This R script has two functions:
+## "makeCacheMatrix": it will create a special matrix 
+##                    with functions to access/update values of the matrix
+## "cacheSolve": it will produce and store inverse of the matrix produced 
+##               by "makeCacheMatrix" function
+
 
 ## This function creates a special "matrix" object that can cache its inverse
 ##
@@ -29,12 +32,12 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         
         ## function to set value of the inverse matrix 
-        setinverse  <- function(inverse) inverseMatrix <<- inverse  # this will cache the value of inverse matrix
+        setinverse  <- function(inverse) inverseMatrix <<- inverse  
         
         ## function get value of the inverse matrix
         getinverse <- function() inverseMatrix
         
-        ## Prepare & Retrun a list of functions created within this(makeCacheMatrix) function
+        ## Prepare & Retrun a list of functions created within this function
         list(set = set, 
              get = get,
              setinverse = setinverse,
@@ -42,30 +45,33 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function computes and caches the inverse of the special "matrix" returned by makeCacheMatrix above. 
-## If the inverse has already been calculated (and the matrix has not changed), it will retrun value from the cache
+## This function computes and caches the inverse of 
+## the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed)
+## it will retrun value from the cache
 ##
-## Input : a list 'x' representing a "special matrix" returned by function "makeCacheMatrix"
+## Input : a list 'x' representing a "special matrix" 
+##         returned by function "makeCacheMatrix"
 ## output: inverse of the input "special matrix" 
 
 cacheSolve <- function(x, ...) {
         
-        inverseMatrix <- x$getinverse()         # get current cached value of inverse matrix in variable "inverseMatrix"
+        inverseMatrix <- x$getinverse()         # get current cached value
         
-        if(!is.null(inverseMatrix)) {           # Check whether inverse matrix is already cached ("inverseMatrix" is not NULL) 
+        if(!is.null(inverseMatrix)) {           # Check whether already cached 
                 
-                message("getting cached data")  # Print info message: as "inverseMatrix" is not NULL, we got cached value
+                message("getting cached data")  # Print info message
                 
-        } else {                                # if condition failed ("inverseMatrix" is NULL) so we need to calculate inverse 
+        } else {                                # Calculate inverse 
                 
-                directMatrix <- x$get()         # get value of the "special matrix"
+                directMatrix <- x$get()         # get value of "special matrix"
                 
-                inverseMatrix <- solve(directMatrix, ...) # calculate inverse and store the value in variable "inverseMatrix" 
+                inverseMatrix <- solve(directMatrix, ...) # calculate inverse
                 
-                x$setinverse(inverseMatrix)     # set(cache) the value of inverse of "special matrix"
+                x$setinverse(inverseMatrix)     # set(cache) value of inverse
                 
         }
         
-        inverseMatrix	                        # retrun inverse of input 'x' the "special matrix"
+        inverseMatrix	                        # retrun inverse
 }
 
